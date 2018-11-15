fabf9754da507daee4f6a04455ed81f075fd6dd4
Spencer Baugh <sbaugh@catern.com>
2018-11-15 01:33:24 +0000

    stuff

--- python/default.nix
+++ python/default.nix
@@ -5,7 +5,8 @@ buildPythonPackage {
   name = "rsyscall";
   src = ./.;
   # doCheck = false;
-  checkInputs = [ mypy pytest ];
+  checkInputs = [ mypy pytest pkgs.openssl ];
+  buildInputs = [ pkgs.openssh ];
   propagatedBuildInputs = [ (import ../c)
       trio cffi pkgconfig python-prctl pkgs.nginx ];
 }
