88fa5713f29cb2217939ba960ceb77ec0fa805b7
Spencer Baugh <sbaugh@catern.com>
2018-12-06 10:12:13 -0500

    foo

--- test_epoll_signalfd.c
+++ test_epoll_signalfd.c
@@ -1,4 +1,6 @@
+#define _GNU_SOURCE
 #include <signal.h>
+#include <sched.h>
 #include <err.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -11,29 +13,38 @@ int try(int ret) {
     return ret;
 }
 
-int main() {
-    int epfd = try(epoll_create1(0));
+void parent(long epfd) {
+    printf("parent pid is %d\n", getpid());
+    sleep(1);
+    printf("raising in parent\n");
+    // try(kill(getpid(), SIGPIPE));
+    printf("calling epoll_wait\n");
+    struct epoll_event receive_event;
+    int events = try(epoll_wait(epfd, &receive_event, 1, -1));
+    printf("got %d events back from epoll_wait\n", events);
+}
+
+int child(void *arg) {
+    printf("child pid is %d\n", getpid());
+    long epfd = (long) arg;
     sigset_t mask = {};
     try(sigaddset(&mask, SIGPIPE));
     try(sigprocmask(SIG_BLOCK, &mask, NULL));
-    pid_t pid = try(fork());
-    if (pid > 0) {
-	printf("child pid is %d\n", pid);
-	printf("parent pid is %d\n", getpid());
-	sleep(1);
-	printf("raising in parent\n");
-	// try(kill(getpid(), SIGPIPE));
-	printf("calling epoll_wait\n");
-	struct epoll_event receive_event;
-	int events = try(epoll_wait(epfd, &receive_event, 1, -1));
-	printf("got %d events back from epoll_wait\n", events);
-    } else {
-	int sigfd = try(signalfd(-1, &mask, SFD_NONBLOCK));
-	struct epoll_event monitor_event = { .events = EPOLLIN, .data = 0 };
-	try(epoll_ctl(epfd, EPOLL_CTL_ADD, sigfd, &monitor_event));
-	printf("signalfd added to epfd\n");
-	printf("raising in child\n");
-	// try(kill(getpid(), SIGPIPE));
-	sleep(1000);
-    }
+    int sigfd = try(signalfd(-1, &mask, SFD_NONBLOCK));
+    struct epoll_event monitor_event = { .events = EPOLLIN, .data = 0 };
+    try(epoll_ctl(epfd, EPOLL_CTL_ADD, sigfd, &monitor_event));
+    printf("signalfd added to epfd\n");
+    printf("raising in child\n");
+    // try(kill(getpid(), SIGPIPE));
+    sleep(100);
+}
+
+int main() {
+    long epfd = try(epoll_create1(0));
+    sigset_t mask = {};
+    void *stack = malloc(4096) + 4096;
+    try(sigaddset(&mask, SIGPIPE));
+    try(sigprocmask(SIG_BLOCK, &mask, NULL));
+    try(clone(child, stack, CLONE_VM|CLONE_SIGHAND, (void *) epfd));
+    parent(epfd);
 }
