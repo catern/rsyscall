2ce62337e9216da5a00f7d6c150ef0fb88d1bc0f
Spencer Baugh <sbaugh@catern.com>
2019-01-15 10:16:01 -0500

    weee

--- python/rsyscall/scripts/local_repl.py
+++ python/rsyscall/scripts/local_repl.py
@@ -5,10 +5,13 @@ import typing as t
 import logging
 
 async def main() -> None:
-    number = await rsc.wish(int)
-    flavor = await rsc.wish(str, "Sorry for being so rude, spirit. Could you tell me your favorite flavor of pie?")
-    pies = [f"A tasty {flavor} pie."]*number
-    await rsc.wish(None, f"Here you go spirit! {number} delicious {flavor} pies! Return when you're done eating them! 'v'")
-    print("Bye spirit! See you later!")
+    games = await rsc.wish(t.List[str], "tell me ur faverut games")
+    for game in games:
+        print(game, "is fun")
+    # number = await rsc.wish(int)
+    # flavor = await rsc.wish(str, "Sorry for being so rude, spirit. Could you tell me your favorite flavor of pie?")
+    # pies = [f"A tasty {flavor} pie."]*number
+    # await rsc.wish(None, f"Here you go spirit! {number} delicious {flavor} pies! Return when you're done eating them! 'v'")
+    # print("Bye spirit! See you later!")
 
 trio.run(main)
