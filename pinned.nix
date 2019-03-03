1c881d323d547c1f8655705f8796f13199d29324
Spencer Baugh <sbaugh@catern.com>
2019-03-03 20:17:33 +0000

    pinnningg

--- pinned.nix
+++ pinned.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchTarball {
-  url = https://github.com/catern/nixpkgs/archive/d5381eef870de608707fcb8439a5fc9d4235deac.tar.gz;
-  sha256 = "1k54hwiysh8pg67cqmrcnw0a49nl3nfvydgfaj3c3zwcygxijd9x";
+  url = https://github.com/catern/nixpkgs/archive/5b3ba5366083ddc7693c81a3cd3077aff5f6b149.tar.gz;
+  sha256 = "1s8wajl9904i2pbs04yh5znzzfsdld152m6yf20r8z5jvvvcpkg3";
 }) {}
--- python/default.nix
+++ python/default.nix
@@ -6,6 +6,13 @@ let
       rev = "ea5510e0499fbd1e264349e86d35ec0078fe73f5";
       sha256 = "0lli1gwwcqbhhm1s6816l33m38irqz1fzjrxb1gjp873d2lddg10";
   };});
+  # hydra = pkgs.hydra;
+  hydra = pkgs.hydra.overrideAttrs (_: { src = pkgs.fetchFromGitHub {
+      owner = "catern";
+      repo = "hydra";
+      rev = "43c62206aba7fb74bc2a08ad707d34a55a88b0fd";
+      sha256 = "0v5qr5gj2wrbnib09qhi9ys4x1wqkr6x44nww4cv5anjjfxaff72";
+  };});
 in
 with pkgs.python37Packages;
 
@@ -19,7 +26,7 @@ pytest ];
   buildInputs = [ pkgs.openssh nix ];
   propagatedBuildInputs = [ (import ../c)
       trio cffi pkgconfig python-prctl pkgs.nginx typeguard
-      requests h11 pkgs.pkgconfig pkgs.hydra
+      requests h11 pkgs.pkgconfig hydra
   ];
 }
 
