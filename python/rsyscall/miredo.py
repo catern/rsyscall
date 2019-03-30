1f75e3c03cb30cbf23dd471fc2c44f1c2a5382b6
Spencer Baugh <sbaugh@catern.com>
2019-03-30 19:46:52 +0000

    notes

--- python/rsyscall/miredo.py
+++ python/rsyscall/miredo.py
@@ -39,6 +39,7 @@ async def start_miredo(nursery, stdtask: StandardTask) -> Miredo:
     await ns_thread.stdtask.unshare_net()
     # we'll keep the ns thread around so we don't have to mess with setns;
     # we'll fork off another thread to actually exec miredo.
+    # miredo doesn't properly clean up its child processes, so run it in a pid namespace
     thread = await ns_thread.stdtask.fork(newpid=True)
     sock = sock.move(thread.stdtask.task.base)
     # hmm
@@ -50,34 +51,53 @@ async def start_miredo(nursery, stdtask: StandardTask) -> Miredo:
     config_fd = await thread.stdtask.task.memfd_create('miredo.conf')
     await config_fd.write_all(config.encode())
     await config_fd.lseek(0, os.SEEK_SET)
-    # TODO miredo doesn't properly clean up its child process, sigh
-    # TODO hmm we probably want to... have our own thread be the parent of this net ns...
-    # maybe? or we could just setns into it...
-    # yeah setnsing into it seems better
-    # ok so we'll unshare user and pid,
-    # then unshare net after opening the socket
-    # open hmm
-    # we want this to move this to be owned by stdtask. hm.
     await thread.stdtask.unshare_files(going_to_exec=True)
     await sock.handle.disable_cloexec()
     await config_fd.handle.disable_cloexec()
-    child = await thread.exec(miredo.args('-f', '-c', config_fd.handle.as_proc_path(), '-u', 'root'))
+    # so for privsep let's just run privproc and main process separately, directly
+    # and put privproc in empty mount namespace and locked down, with inherited cap
+
+    # okay so I guess we'll bind the tunnel,
+    # and then create the privproc to manage it,
+    # and pass those down to the minimal miredo daemon
+    # ok so that's easy enough
+    # and then we don't need the pid namespace
+    # okay so it reloads configuration by just killing the process and restarting. lol
+    # okay so we should be able to cut this down a lot
+    miredo_command = miredo.args('-f', '-c', config_fd.handle.as_proc_path(), '-u', 'root')
+    print("command is", miredo_command.in_shell_form())
+    child = await thread.exec(miredo_command)
+    # child = await thread.exec(bash)
     nursery.start_soon(child.check)
     return Miredo(ns_thread)
 
 class TestMiredo(TrioTestCase):
     async def asyncSetUp(self) -> None:
+        # TODO lmao stracing this stuff causes a bug,
+        # waiting a long time between runs causes a bug,
+        # what is even going on
         self.stdtask = rsc.local_stdtask
         self.miredo = await start_miredo(self.nursery, self.stdtask)
 
     async def test_miredo(self) -> None:
         ping6 = await rsc.which(self.stdtask, "ping6")
         # TODO properly wait for miredo to be up...
+        # Maybe there's a way to wait for a specific interface to be up?
+        # We could just wait for that?
+        # Right, I can just run "ip monitor".
+        # I *could* use netlink directly, but that seems like a hassle.
+        # can I filter it?
+        # o kaaay, so I think I can use netlink directly.
+        # the manpage is extremely incomplete
+        # I think it might be worth using pyroute2 rather than reimplementing the parsing stuff.
+        # hopefully they are a little flexible about how they build messages
         await trio.sleep(.1)
         # ping needs root, so let's fork it off from the
         # miredo-ns-thread, which has root in the namespace
         thread = await self.miredo.ns_thread.stdtask.fork()
-        await thread.run(ping6.args('-c', '1', 'google.com'))
+        # bash = await rsc.which(self.stdtask, "bash")
+        # await (await thread.exec(bash)).check()
+        await (await thread.exec(ping6.args('-c', '1', 'google.com'))).check()
 
 if __name__ == "__main__":
     import unittest
