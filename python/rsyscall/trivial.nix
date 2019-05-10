a73374765a424add379baf222bd634a6a2686c20
Spencer Baugh <sbaugh@catern.com>
2019-05-10 08:12:32 -0400

    delete some stray files

--- python/rsyscall/trivial.nix
+++
@@ -1,8 +0,0 @@
-{ string }:
-{ trivial = builtins.derivation {
-    name = "trivial";
-    system = "x86_64-linux";
-    builder = "/bin/sh";
-    args = ["-c" "echo ${string} > $out; exit 0"];
-  };
-}
