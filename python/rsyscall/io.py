ac3c2540bf3a147e5161c3d5ffb14f7c8c905a99
Spencer Baugh <sbaugh@catern.com>
2019-05-10 16:08:26 +0000

    move make_connections out

--- python/rsyscall/io.py
+++ python/rsyscall/io.py
@@ -42,7 +42,7 @@ from rsyscall.signal import HowSIG, Sigaction, Sighandler, Signals, Sigset, Sigi
 from rsyscall.signal import SignalBlock
 from rsyscall.linux.dirent import Dirent, DirentList
 from rsyscall.unistd import SEEK
-from rsyscall.network.connection import Connection
+from rsyscall.network.connection import Connection, make_connections
 
 import random
 import string
@@ -565,9 +565,12 @@ class StandardTask:
             t.Tuple[handle.FileDescriptor, handle.FileDescriptor]
     ]:
         return (await make_connections(
-            self.access_task, self.access_connection,
-            self.connecting_task, self.connecting_connection,
-            self.task, count))
+            self.access_task.task, self.access_task,
+            self.access_connection,
+            self.connecting_task.task, self.connecting_task,
+            self.connecting_connection,
+            self.task.task, self.task,
+            count))
 
     async def fork(self, newuser=False, newpid=False, fs=True, sighand=True) -> RsyscallThread:
         [(access_sock, remote_sock)] = await self.make_async_connections(1)
@@ -858,72 +861,6 @@ async def unshare_files(
     # perform a cloexec
     if not going_to_exec:
         await do_cloexec_except(task, process_resources, copy_to_new_space)
-
-async def make_connections(access_task: Task,
-                           # regrettably asymmetric...
-                           # it would be nice to unify connect/accept with passing file descriptors somehow.
-                           access_connection: t.Optional[t.Tuple[Path, handle.FileDescriptor]],
-                           connecting_task: Task,
-                           connecting_connection: t.Tuple[handle.FileDescriptor, handle.FileDescriptor],
-                           parent_task: Task,
-                           count: int) -> t.List[t.Tuple[handle.FileDescriptor, handle.FileDescriptor]]:
-    # so there's 1. the access task, through which we access the syscall and data fds,
-    # 2. the parent task, and
-    # 3. the connection between the access and parent task, so that we can have the parent task pass down the fds,
-    # while the access task uses them.
-    # okay but this is a slight simplification, because there may also be,
-    # 4. the connection task, which is a task that actually gets the fds and passes them down to the parent task
-    access_socks: t.List[handle.FileDescriptor] = []
-    connecting_socks: t.List[handle.FileDescriptor] = []
-    if access_task.base.fd_table == connecting_task.base.fd_table:
-        async def make_conn() -> t.Tuple[handle.FileDescriptor, handle.FileDescriptor]:
-            pair = await (await access_task.base.socketpair(
-                AF.UNIX, SOCK.STREAM, 0, await access_task.malloc_struct(handle.FDPair))).read()
-            return (pair.first, pair.second)
-    else:
-        if access_connection is not None:
-            access_connection_path, access_connection_socket = access_connection
-        else:
-            raise Exception("must pass access connection when access task and connecting task are different")
-        async def make_conn() -> t.Tuple[handle.FileDescriptor, handle.FileDescriptor]:
-            left_sock = await access_task.base.socket(AF.UNIX, SOCK.STREAM)
-            async with access_connection_path.as_sockaddr_un() as addr:
-                await left_sock.connect(await access_task.to_pointer(addr))
-            right_sock = await access_connection_socket.accept(SOCK.CLOEXEC)
-            return left_sock, right_sock
-    for _ in range(count):
-        access_sock, connecting_sock = await make_conn()
-        access_socks.append(access_sock)
-        connecting_socks.append(connecting_sock)
-    passed_socks: t.List[handle.FileDescriptor]
-    if connecting_task.base.fd_table == parent_task.base.fd_table:
-        passed_socks = []
-        for sock in connecting_socks:
-            passed_socks.append(sock.move(parent_task.base))
-    else:
-        assert connecting_connection is not None
-        def sendmsg_op(sem: batch.BatchSemantics) -> handle.WrittenPointer[handle.SendMsghdr]:
-            iovec = sem.to_pointer(handle.IovecList([sem.malloc_type(Bytes, 1)]))
-            cmsgs = sem.to_pointer(handle.CmsgList([handle.CmsgSCMRights([sock for sock in connecting_socks])]))
-            return sem.to_pointer(handle.SendMsghdr(None, iovec, cmsgs))
-        _, [] = await connecting_connection[0].sendmsg(await connecting_task.perform_batch(sendmsg_op), SendmsgFlags.NONE)
-        def recvmsg_op(sem: batch.BatchSemantics) -> handle.WrittenPointer[handle.RecvMsghdr]:
-            iovec = sem.to_pointer(handle.IovecList([sem.malloc_type(Bytes, 1)]))
-            cmsgs = sem.to_pointer(handle.CmsgList([handle.CmsgSCMRights([sock for sock in connecting_socks])]))
-            return sem.to_pointer(handle.RecvMsghdr(None, iovec, cmsgs))
-        _, [], hdr = await connecting_connection[1].recvmsg(await parent_task.perform_batch(recvmsg_op), RecvmsgFlags.NONE)
-        cmsgs_ptr = (await hdr.read()).control
-        if cmsgs_ptr is None:
-            raise Exception("cmsgs field of header is, impossibly, None")
-        [cmsg] = await cmsgs_ptr.read()
-        if not isinstance(cmsg, handle.CmsgSCMRights):
-            raise Exception("expected SCM_RIGHTS cmsg, instead got", cmsg)
-        passed_socks = cmsg
-        # don't need these in the connecting task anymore
-        for sock in connecting_socks:
-            await sock.close()
-    ret = list(zip(access_socks, passed_socks))
-    return ret
 
 class RsyscallThread:
     def __init__(self,
