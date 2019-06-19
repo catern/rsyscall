231292e99abdabe38867c9ce7685a943a14a9b52
Spencer Baugh <sbaugh@catern.com>
2019-06-19 01:19:06 +0000

    python: set version to the correct 0.0.1

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='rsyscall',
-      version='0.1.0',
+      version='0.0.1',
       description='A library for making system calls remotely, through another process, which may be located on a remote host',
       classifiers=[
           "Programming Language :: Python :: 3",
