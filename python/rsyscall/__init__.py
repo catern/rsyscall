03c5861e7ecfcb5cebd4ec9d39a2043d60ae1c59
Spencer Baugh <sbaugh@catern.com>
2019-08-20 16:23:27 +0000

    make some things importable directly with "from rsyscall import ..."
    This should be a useful convenience. This leads to a natural workflow
    where generally useful rsyscall-specific classes are imported from
    "rsyscall", and classes/enums which are specific to specific syscalls
    are imported from the modules whose paths mimic the headers;
    i.e. "from rsyscall.sys.socket import AF".

--- python/rsyscall/__init__.py
+++ python/rsyscall/__init__.py
@@ -1,1 +1,8 @@
 "rsyscall is a library for Linux."
+from rsyscall.thread import Thread, ChildThread
+from rsyscall.command import Command
+from rsyscall.path import Path
+from rsyscall.handle import FileDescriptor
+from rsyscall.handle import WrittenPointer, Pointer
+from rsyscall.epoller import AsyncFileDescriptor
+from rsyscall.monitor import AsyncChildProcess
