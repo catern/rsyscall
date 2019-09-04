3d02a2676da4b2212863cd7302a9dc969dd7bb1d
Spencer Baugh <sbaugh@catern.com>
2019-09-04 16:05:49 +0000

    test_pipe: use malloc/ptr directly from RAMThread

--- python/rsyscall/tests/test_pipe.py
+++ python/rsyscall/tests/test_pipe.py
@@ -8,21 +8,21 @@ from rsyscall.fcntl import O
 class TestPipe(TrioTestCase):
     async def asyncSetUp(self) -> None:
         self.thr = local.thread
-        self.pipe = await (await self.thr.task.pipe(await self.thr.ram.malloc(Pipe))).read()
+        self.pipe = await (await self.thr.task.pipe(await self.thr.malloc(Pipe))).read()
 
     async def asyncTearDown(self) -> None:
         await self.pipe.read.close()
         await self.pipe.write.close()
 
     async def test_read_write(self):
-        in_data = await self.thr.ram.ptr(b"hello")
+        in_data = await self.thr.ptr(b"hello")
         written, _ = await self.pipe.write.write(in_data)
         valid, _ = await self.pipe.read.read(written)
         self.assertEqual(in_data.value, await valid.read())
 
     async def test_readv_writev(self):
         in_data = [b"hello", b"world"]
-        iov = await self.thr.ram.ptr(IovecList([await self.thr.ram.ptr(data) for data in in_data]))
+        iov = await self.thr.ptr(IovecList([await self.thr.ptr(data) for data in in_data]))
         written, partial, rest = await self.pipe.write.writev(iov)
         read, partial, rest = await self.pipe.read.readv(written)
         self.assertEqual(in_data, [await ptr.read() for ptr in read.value])
@@ -34,7 +34,7 @@ class TestPipe(TrioTestCase):
         messing with O_NONBLOCK stuff
 
         """
-        in_data = await self.thr.ram.ptr(b"hello")
+        in_data = await self.thr.ptr(b"hello")
         written, _ = await self.pipe.write.write(in_data)
         with self.assertRaises(OSError):
             valid, _ = await self.pipe.read.recv(written, 0)
