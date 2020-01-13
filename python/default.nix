696b22f01eb1d4584459d0c18cd75a877ab9bb57
Spencer Baugh <sbaugh@catern.com>
2020-01-13 21:20:45 +0000

    update default.nix to exclude tests broken in Nix sandbox

--- python/default.nix
+++ python/default.nix
@@ -30,12 +30,16 @@ with pkgs.python37Packages;
 buildPythonPackage {
   name = "rsyscall";
   src = ./.;
-  # doCheck = false;
   checkInputs = [
   pydocstyle
   (mypy.overrideAttrs (_: { src = /home/sbaugh/.local/src/mypy; }))
   typing-extensions
 pytest ];
+  # ssh tests don't work because the build user's login shell is /noshell :(
+  # net tests don't work because /dev/net/tun doesn't exist
+  # nix tests don't work because something about "error: creating directory '/nix/var': Permission denied"
+  # test_pgid doesn't work because /proc/sys/kernel/ns_last_pid isn't available for some reason
+  checkPhase = "pytest rsyscall -k 'not ssh and not test_net and not test_nix and not test_pgid'";
   nativeBuildInputs = [
       pkgs.pkg-config pkgs.openssh nix
       rsyscall
