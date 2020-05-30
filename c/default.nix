3566be1e553a8eb6927139708c9c3ecf97c16aa8
Spencer Baugh <sbaugh@catern.com>
2020-05-30 17:01:02 -0400

    c: fix static compilation of rsyscall-bootstrap executable

--- c/default.nix
+++ c/default.nix
@@ -3,5 +3,9 @@ with import ../pinned.nix;
 stdenv.mkDerivation {
   name = "rsyscall";
   src = ./.;
-  buildInputs = [ autoreconfHook autoconf automake libtool pkgconfig];
+  # rsyscall needs to build some static bootstrap binaries; that requires its library be built
+  # statically in addition to dynamically.
+  # we'll install both static and dynamic libraries, which is fine.
+  dontDisableStatic = true;
+  buildInputs = [ autoreconfHook autoconf automake libtool pkgconfig glibc glibc.static];
 }
