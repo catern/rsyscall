28f2e0e8155fa3760b1e0b06d925a69fd303f8c0
Spencer Baugh <sbaugh@catern.com>
2020-09-01 20:01:48 -0400

    rsyscall: export more types from the top-level module
    In general, if a type wouldn't exist in C, or would be available
    automatically, we should put it in the main rsyscall module.

--- python/rsyscall/__init__.py
+++ python/rsyscall/__init__.py
@@ -2,8 +2,13 @@
 from rsyscall.thread import Thread, ChildThread
 from rsyscall.command import Command
 from rsyscall.path import EmptyPath, Path
-from rsyscall.handle import FileDescriptor
-from rsyscall.handle import WrittenPointer, Pointer
+from rsyscall.handle import (
+    FileDescriptor, Task,
+    WrittenPointer, Pointer,
+)
 from rsyscall.epoller import AsyncFileDescriptor
 from rsyscall.monitor import AsyncChildProcess
 from rsyscall.unistd import Arg
+from rsyscall.struct import Int32, Int64
+from rsyscall.tasks.local import thread as local_thread
+from rsyscall.sys.mman import MemoryMapping
