c048e0efe7417c722453fb166c7c4ed486922862
Spencer Baugh <sbaugh@catern.com>
2020-09-05 18:30:13 -0400

    DEV: use local nixpkgs

--- pinned.nix
+++ pinned.nix
@@ -1,4 +1,5 @@
-import (builtins.fetchTarball {
-  url = https://github.com/nixos/nixpkgs/archive/045384ac98ced4f34b735a9dc56c974759d2d52c.tar.gz;
-  sha256 = "1vb0bshhbvxhc7lf1wjhn87g3v51rkjcnqvbdb3a7sz2yw46pyd3";
-}) {}
+import /home/sbaugh/.local/src/nixpkgs {}
+# import (builtins.fetchTarball {
+#   url = https://github.com/nixos/nixpkgs/archive/045384ac98ced4f34b735a9dc56c974759d2d52c.tar.gz;
+#   sha256 = "1vb0bshhbvxhc7lf1wjhn87g3v51rkjcnqvbdb3a7sz2yw46pyd3";
+# }) {}
