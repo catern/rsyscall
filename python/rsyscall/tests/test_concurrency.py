7c556bba2b472b80b163a6c4d6636c9c085cf236
Spencer Baugh <sbaugh@catern.com>
2020-10-05 11:45:45 -0400

    test_concurrency: add sanity check for trio cancelscope behavior

--- python/rsyscall/tests/test_concurrency.py
+++ python/rsyscall/tests/test_concurrency.py
@@ -28,3 +28,10 @@ class TestConcurrency(TrioTestCase):
                     nursery.cancel_scope.cancel()
             nursery.start_soon(a1)
             nursery.start_soon(a2)
+
+    async def test_nest_cancel_inside_shield(self) -> None:
+        "If we cancel_scope.cancel() inside a CancelScope which is shielded, it works."
+        with trio.CancelScope(shield=True):
+            async with trio.open_nursery() as nursery:
+                nursery.start_soon(trio.sleep_forever)
+                nursery.cancel_scope.cancel()
