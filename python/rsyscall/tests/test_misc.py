5db8f6615aff0d8279c85e1082c624e2965953f1
Spencer Baugh <sbaugh@catern.com>
2021-01-07 16:05:56 -0500

    test_misc: don't pointlessly call rsyscall_exec
    This just makes the test more complicated - we can just do it in a
    different fd table, we don't need to exec.

--- python/rsyscall/tests/test_misc.py
+++ python/rsyscall/tests/test_misc.py
@@ -3,7 +3,6 @@ import unittest
 
 from rsyscall.tests.trio_test_case import TrioTestCase
 from rsyscall.nix import local_store
-from rsyscall.tasks.exec import *
 
 from rsyscall import local_thread
 
@@ -18,16 +17,12 @@ class TestMisc(TrioTestCase):
     async def asyncSetUp(self) -> None:
         self.local = local_thread
         self.store = local_store
-        self.executables = await RsyscallServerExecutable.from_store(self.store)
-        self.thr = await self.local.clone(CLONE.FILES)
+        self.thr = await self.local.clone()
 
     async def asyncTearDown(self) -> None:
         await self.thr.close()
 
     async def test_do_cloexec_except(self) -> None:
-        # do_cloexec_except breaks trio when run locally
-        await rsyscall_exec(self.thr, self.executables)
-
         pipe = await (await self.thr.task.pipe(await self.thr.ram.malloc(Pipe))).read()
         close_set = set([fd.near for fd in self.thr.task.fd_handles])
         close_set.remove(pipe.read.near)
