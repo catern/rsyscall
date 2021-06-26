7bb0d9b933eddc33e0406264bc074a19c81d51c9
George Shammas <george@shamm.as>
2021-06-25 21:17:00 -0400

    Nix flake support
    Signed-off-by: George Shammas <george@shamm.as>

--- pinned.nix
+++ pinned.nix
@@ -1,4 +1,21 @@
-import (builtins.fetchTarball {
-  url = https://github.com/nixos/nixpkgs/archive/d3f7e969b9860fb80750147aeb56dab1c730e756.tar.gz;
-  sha256 = "13z5lsgfgpw2wisglicy7krjrhypcc2y7krzxn54ybcninyiwhsn";
-}) {}
+# Super stripped down version of nixpkgs compat
+# https://github.com/edolstra/flake-compat/blob/master/default.nix
+
+let
+  lockFile = builtins.fromJSON (builtins.readFile ./flake.lock);
+  fetchTree = info:
+    if info.type == "github" then
+      {
+        outPath = builtins.fetchTarball "https://api.${info.host or "github.com"}/repos/${info.owner}/${info.repo}/tarball/${info.rev}";
+        rev = info.rev;
+        shortRev = builtins.substring 0 7 info.rev;
+        lastModified = info.lastModified;
+        narHash = info.narHash;
+      }
+    else
+      throw "flake input has unsupported input type '${info.type}'";
+in
+import (fetchTree lockFile.nodes.nixpkgs.locked) {
+  config = { };
+  overlays = [ (import ./overlay.nix) ];
+}
---
+++ python/package.nix
@@ -0,0 +1,85 @@
+{ pythonPackages
+, librsyscall
+, nix
+, socat
+, pkg-config
+, s6
+, miredo
+, postgresql_11
+, iproute
+, openssh
+, opensmtpd
+, dovecot
+, hydra
+, powerdns
+, bubblewrap
+, nginx
+, bash
+, coreutils
+, hello
+}:
+
+with pythonPackages;
+buildPythonPackage {
+  name = "rsyscall";
+  src = ./.;
+  checkInputs = [
+    pydocstyle
+    mypy
+    typing-extensions
+    pytest
+    socat
+  ];
+  # ssh tests don't work because the build user's login shell is /noshell :(
+  # net tests don't work because /dev/net/tun doesn't exist
+  # nix tests don't work because something about "error: creating directory '/nix/var': Permission denied"
+  # test_pgid doesn't work because /proc/sys/kernel/ns_last_pid isn't available for some reason
+  # fuse tests don't work because /dev/fuse doesn't exist
+  checkPhase = "pytest rsyscall -k 'not ssh and not test_net and not test_nix and not test_pgid and not test_fuse'";
+  nativeBuildInputs = [
+    pkg-config
+    openssh
+    nix
+    librsyscall
+    (pdoc3.overridePythonAttrs (_: { doCheck = false; }))
+  ];
+  # not sure how to set up the deps. we use binaries and libraries from C
+  # rsyscall at build time to run tests; and we also use them at runtime for our
+  # actual functionality. so should rsyscall be in nativeBuildInputs or
+  # buildInputs? strictDeps fails if it's in nativeBuildInputs...
+  strictDeps = false;
+  nativePropagatedBuildInputs = [
+    s6
+    # miredo
+    postgresql_11
+    iproute
+    # opensmtpd
+    dovecot
+    # hydra
+    powerdns
+    bubblewrap
+    nginx
+  ];
+  buildInputs = [
+    cffi
+  ];
+  propagatedBuildInputs = [
+    trio
+    typeguard
+    h11
+    dnspython
+    pyroute2
+    outcome
+  ];
+  inherit miredo nix librsyscall openssh bash coreutils hello;
+  rsyscall = librsyscall;
+  exportReferencesGraph =
+    [ "miredo" miredo ]
+    ++ [ "nix" nix ]
+    ++ [ "rsyscall" librsyscall ]
+    ++ [ "openssh" openssh ]
+    ++ [ "bash" bash ]
+    ++ [ "coreutils" coreutils ]
+    ++ [ "hello" hello ];
+}
+
