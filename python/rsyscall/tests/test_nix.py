de69c474bf39936f0fd7209e7c99b0fe90465c5b
Spencer Baugh <sbaugh@catern.com>
2022-02-07 14:02:41 -0500

    test_nix: don't uselessly print "hello world" to stdout

--- python/rsyscall/tests/test_nix.py
+++ python/rsyscall/tests/test_nix.py
@@ -8,19 +8,19 @@ import rsyscall._nixdeps.nix
 class TestNix(TrioTestCase):
     async def asyncSetUp(self) -> None:
         self.parent, self.thr = self.thr, await self.thr.fork()
-        self.tmpdir = await mkdtemp(self.parent)
+        self.tmpdir = await mkdtemp(self.parent, "test_nix")
         await enter_nix_container(self.parent, rsyscall._nixdeps.nix.closure, self.thr, self.tmpdir)
 
     async def asyncTearDown(self) -> None:
         await self.tmpdir.cleanup()
 
-    async def test_hello(self) -> None:
-        hello = (await deploy(self.thr, rsyscall._nixdeps.coreutils.closure)).bin('echo').args('hello world')
-        await self.thr.run(hello)
+    async def test_true(self) -> None:
+        true = (await deploy(self.thr, rsyscall._nixdeps.coreutils.closure)).bin('true')
+        await self.thr.run(true)
 
     async def test_with_daemon(self) -> None:
         nix_daemon = (await deploy(self.thr, rsyscall._nixdeps.nix.closure)).bin("nix-daemon")
         nd_child = await (await self.thr.fork()).exec(nix_daemon)
         self.thr.environ['NIX_REMOTE'] = 'daemon'
-        hello = (await deploy(self.thr, rsyscall._nixdeps.coreutils.closure)).bin('echo').args('hello world')
-        await self.thr.run(hello)
+        true = (await deploy(self.thr, rsyscall._nixdeps.coreutils.closure)).bin('true')
+        await self.thr.run(true)
